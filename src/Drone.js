75be0771f77eee3fb313da475dfaa0f48dbbc45c
Constantin Clauzel <stantin04@gmail.com>
2015-07-18 21:58:35 +0200

    impending try running code

--- src/Drone.js
+++ src/Drone.js
@@ -2,177 +2,253 @@
 
 import _ from "lodash";
 
+var debug = function(o) {
+    if (this.options.debug) {
+      console.log(o);
+    }
+};
+
+var definitions = [];
+
+ // definitionsById cache
+var definitionsById = {};
+var getDefinitionsById = function(objectId) {
+    var definition = getDefinitionsById[objectId];
+
+    if (_.isUndefined(definition)) {
+      throw "Unknown Definition Exception -> " + name;
+    }
+    return definition;
+};
+
+ // definitionsByName cache
+var definitionsByName = {};
+var getDefinitionsByName = function(name) {
+    var definition = definitionsByName[name];
+
+    if (_.isUndefined(definition)) {
+      throw "Unknown Definition Exception -> " + name;
+    }
+    return definition;
+};
+
+// definitions manipulation
+function addDefinition(definition) {
+    definitions.push(definition);
+    definitionsById = _.indexBy(definitions, 'id');
+    definitionsByName = _.indexBy(definitions, 'name');
+}
+
+/**
+ * general purpose handler manager
+ */
+var newHandlerManager = function() {
+
+    // Handlers that will be attached to UavObjects
+    var handlers = {};
+
+    var detachHandlerAtIndex = function(name, index) {
+        if (_.contains(_.keys(definitionsByName), name) == false) {
+            var handlers =  this.handlers[name];
+            handlers.splice(index--, 1);
+            if (handlers.length == 0) {
+                this.handlers[name] = null;
+                this.sendUnsubsribe(name);
+            }
+        } else {
+            throw("Unknown Uav handler");
+        }
+    };
+
+    return {
+        callHandlers: function(name) {
+            // Dispatch events to their callbacks
+            if (handlers[uavObject.name]) {
+              var handlers = handlers[uavObject.name];
+
+              for (var i = 0; i < handlers.length; i++) {
+                var callback  = handlers[i][0];
+                var callCount = handlers[i][1];
+
+                if (callCount > 0) {  // it's not a permanent callback
+                  if (--handlers[i][1] == 0) { // did it consumed all its allowed calls ?
+                    debug("Detaching consumed callback from " + uavObject.name);
+                    detachHandlerAtIndex(uavObject.name, i);
+                  }
+                }
+                callback(uavObject);
+              }
+            }
+        },
+
+        // TODO use promises, ASAP
+        attachHandler: function(name, callback, callCount) {
+          if (callCount == undefined)
+            callCount = -1;
+
+          if (_.contains(_.keys(definitionsByName), name)) {
+            if (this.handlers[name] === undefined) {
+              this.handlers[name] = [];
+              this.sendSubscribe(name);
+            }
+
+            this.handlers[name].push([callback, callCount]);
+          } else {
+            throw("Unknown Uav handler");
+          }
+      },
+
+        detachHandler: function(name, callback) {
+            if (_.contains(_.keys(definitionsByName), name) == false) {
+                var handlers =  this.handlers[name];
+                for (var i = 0; i < handlers.length; i++) {
+                    var cb  = handlers[i][0];
+                    if (cb == callback) {
+                        detachHandlerAtIndex(name, i);
+                    }
+                }
+            } else {
+                throw("Unknown Uav handler");
+            }
+        },
+
+        attachOnce: function(name, callback) {
+          this.attachHandler(name, callback, 1);
+        }
+    }
+};
+
+var sendUpdate = function(name, data) {
+  var definition = getDefinitionsByName(name);
+
+  this.socket.send(JSON.stringify({
+      type: Drone.REQUEST_TYPES.UPDATE,
+      payload: {
+          objectId: definition.id,
+          instanceId: 0,
+          data: data
+      }
+  }));
+};
+
+var sendRequest = function(name, instanceId) {
+    var definition = getDefinitionsByName(name);
+
+    this.socket.send(JSON.stringify({
+      type: Drone.REQUEST_TYPES.REQUEST,
+      payload: {
+          objectId: definition.id,
+          instanceId: instanceId
+      }
+  }));
+};
+
+var sendSubsribe = function(name) {
+    var definition = getDefinitionsByName(name);
+
+    this.socket.send(JSON.stringify({
+      type: Drone.REQUEST_TYPES.SUBSCRIBE,
+      payload: {
+          objectId: definition.id
+      }
+  }));
+};
+
+var sendUnsubsribe = function(name) {
+    var definition = getDefinitionsByName(name);
+
+    this.socket.send(JSON.stringify({
+        type: Drone.REQUEST_TYPES.UNSUBSCRIBE,
+        payload: {
+            objectId: definition.id
+        }
+    }));
+};
+
 export class Drone {
   constructor(url, options) {
     this.url = url;
-    this.telemetryReady = false;
+
     this.readyCallbacks = [];
 
     this.options = {
       debug: false
     }
-
-    // Handlers that will be attached to UavObjects
-    this.handlers = {};
-
     _.extend(this.options, options);
+
+    this.updateHandlers = newHandlerManager();
+    this.requestHandlers = newHandlerManager();
+    this.definitionHandlers = newHandlerManager();
   }
 
   connect() {
     this.socket = new WebSocket(this.url);
     this.socket.onmessage = _.bind(this.handleMessage, this);
 
-    this.onDefinitionsLoaded = _.bind(function() {
-      this.setupTelemetry();
-    }, this);
-
-    this.connected = true
-      return;
+    this.socket.ononopen = function(event) {
+        this.connected = true;
+        _.forEach(this.readyCallbacks, function(index, readyCallback) {
+            readyCallback();
+        });
+    };
   }
 
   isConnected() {
     return this.connected;
   }
 
-  readUav(name, callback) { 
-    this.sendMessage(name, 'objectRequest', {});
-    this.attachOnce(name, callback);
+  readUav(name, callback) {
+    sendRequest(name);
+    this.updateHandlers.attachOnce(name, callback);
   }
 
   handleMessage(event) {
-    var uavObject = JSON.parse(event.data);
+    var packet = JSON.parse(event.data);
 
-    // First message is always an array of objects definitions
-    if (_.isEmpty(this.uavObjectDefinitionsByName)) {
-      this.uavObjectDefinitionsById = _.indexBy(uavObject, 'objectId');
-      this.uavObjectDefinitionsByName = _.indexBy(uavObject, 'name');
-
-      this.debug("Definitions loaded");
-      this.onDefinitionsLoaded();
-
-      return;
-    }
-
-    // Dispatch events to their callbacks
-    if (this.handlers[uavObject.name]) {
-      var handlers = this.handlers[uavObject.name]; 
-
-      for (var i = 0; i < handlers.length; i++) {
-        var callback  = handlers[i][0];
-        var callCount = handlers[i][1];
-
-        if (callCount > 0) {  // it's not a permanent callback
-          if (--handlers[i][1] == 0) { // did it consumed all its allowed calls ?
-            this.debug("Detaching consumed callback from " + uavObject.name);
-            handlers.splice(i--, 1);
-          }
+    if (packet.type == Drone.REQUEST_TYPES.UPDATE) {
+        var update = packet.payload;
+        var objectId = update.objectId;
+        var definition = definitionsById[objectId];
+        if (_.isUndefined(definition) == false) {
+            this.updateHandlers.callHandlers(definition.name);
         }
+    } else if (packet.type == Drone.REQUEST_TYPES.REQUEST) {
+        var request = packet.payload;
+        var objectId = request.objectId;
+        var definition = definitionsById[objectId];
+        if (_.isUndefined(definition) == false) {
+            this.requestHandlers.callHandlers(definition.name);
+        }
+    } else if (packet.type == Drone.REQUEST_TYPES.DEFINITION) {
+        var definition = packet.payload;
+        addDefinition(definition);
 
-        callback(uavObject);
-      }
-    }
-
-    return;
-  }
-
-  sendMessage(name, requestType, data) {
-    var encodedRequestType  = Drone.REQUEST_TYPES[requestType];
-    var uavObjectDefinition = this.uavObjectDefinitionsByName[name];
-
-    if (_.isUndefined(uavObjectDefinition)) {
-      throw "Unknown UAVObject Exception";
-    }
-
-    this.socket.send(JSON.stringify({
-      objectId: uavObjectDefinition.id,
-      cmd: encodedRequestType,
-      data: data
-    }));
-  }
-
-  setupTelemetry() {
-    this.debug("Handshaking Telemetry stats");
-
-    var that = this;
-    var handshake = function(status) {
-      that.sendMessage('GCSTelemetryStats', 'object', {
-        Status: status,
-        TxDataRate: 0,
-        RxDataRate: 0,
-        TxFailures: 0,
-        RxFailures: 0,
-        TxRetries: 0
-      });
-    };
-
-    handshake('HandshakeReq');
-
-    var debug = _.bind(this.debug, this);
-    // TODO is this really an uavobject ? or is this just a response ?
-
-    this.attachHandler('FlightTelemetryStats', function(uavObjectResponse) {
-      switch (uavObjectResponse.data.Status) {
-        case 'HandshakeAck':
-          debug('Received HandshakeAck, sending Connected');
-          handshake('Connected');
-          break;
-        case 'Disconnected':
-          debug('Received Disconnected, sending HandshakeReq');
-          handshake('HandshakeReq'); // start over
-          break;
-        case 'Connected':
-          debug('Received Connected, sending HandshakeConnected');
-          handshake('Connected');    // just in case (TODO is that really necessary?)
-
-          if (!that.telemetryReady) {
-            that.telemetryReady = true;
-
-            that.readyCallbacks.forEach(function(callback) {
-              callback();
-            });
-          }
-          break;
-        default:
-          debug('Received unknown data\n' + uavObjectResponse.data.Status)
-            throw("Unknown UavObjectResponse Status");
-      }
-    }, -1);
-
-    // this handshake is a bit more specific than the others
-    // as it involves FlightTelemetryStats and GCSTelemetryStats.
-
-    // send GCSTelemetryStats HandshakeReq
-    // recv FlightTelemetryStats
-    // if disconnected
-    //  send GCSTelemetryStats HandshakeReq # restart
-    // elsif HandshakeAck || Connected
-    //github.com/openflylab/drone.js/commit/9b0086
-    //  send GCSTelemetryStats Connected
-    //
-    // heartbeat(period: 5) {
-    //  send FlightStatus       // TODO wtf
-    //  send ActuatorCommand    // TODO wtf
-    // }
-  }
-
-  // TODO use promises, ASAP
-  attachHandler(name, callback, callCount) {
-    if (callCount == undefined)
-      callCount = -1;
-
-    if (_.contains(_.keys(this.uavObjectDefinitionsByName), name)) {
-      if (this.handlers[name] === undefined) {
-        this.handlers[name] = [];
-      }
-
-      this.handlers[name].push([callback, callCount]);
-    } else {
-      throw("Unknown Uav handler");
+        this.definitionHandlers.callHandlers(definition.name);
     }
   }
 
-  attachOnce(name, callback) {
-    this.attachHandler(name, callback, 1);
+  attachUpdateHandler(name, callback, callCount) {
+      this.updateHandlers.attachHandler(name, callback, callCount);
+  }
+
+  detachUpdateHandler(name, callback) {
+      this.updateHandlers.detachHandler(name, callback);
+  }
+
+  attachRequestHandler(name, callback, callCount) {
+      this.requestHandlers.attachHandler(name, callback, callCount);
+  }
+
+  detachRequestHandler(name, callback) {
+      this.requestHandlers.detachHandler(name, callback);
+  }
+
+  attachDefinitionHandler(name, callback, callCount) {
+      this.definitionHandlers.attachHandler(name, callback, callCount);
+  }
+
+  detachDefinitionHandler(name, callback) {
+      this.definitionHandlers.detachHandler(name, callback);
   }
 
   onReady(callback) {
@@ -181,12 +257,12 @@ export class Drone {
 
   debug(o) {
     if (this.options.debug) {
-      console.log(o);
+      debug(o);
     }
   }
 
   defaultValues(uavObjectName) {
-    let definition = this.uavObjectDefinitionsByName[uavObjectName];
+    let definition = definitionsByName[uavObjectName];
     let values = {};
 
     for(let field of definition.fields) {
@@ -226,10 +302,10 @@ export class Drone {
   }
 }
 
-Drone.REQUEST_TYPES = {
-  'object': 0,
-  'objectRequest': 1,
-  'objectAck': 2,
-  'ack': 3,
-  'nack': 4
+Drone.PACKET_TYPES = {
+    UPDATE: 'update',
+    REQUEST: 'req',
+    DEFINITION: 'def',
+    SUBSCRIBE: 'sub',
+    UNSUBSCRIBE: 'unsub'
 }
