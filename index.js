65a2cc02c01b38982766a9f3a193f09109da0d1a
Constantin Clauzel <stantin04@gmail.com>
2015-07-19 20:34:15 +0200

    removed Drone.js, renammed namespace to skybot instead of Drone

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {Client} from "./src/Client.js"
 
 // Is that the proper way to do it given index.js is made to be used only by the browser?
-window.Drone = {
+window.skybot = {
     Client
 };
