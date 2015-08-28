79dd06963799c7816699df9e308eb0cfba231a05
Constantin Clauzel <stantin04@gmail.com>
2015-08-28 11:32:59 +0200

    changed package.json main to src/Client.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Client} from "./src/Client.js"
+import Client from "./src/Client.js"
 
 // Is that the proper way to do it given index.js is made to be used only by the browser?
 window.skybot = {
